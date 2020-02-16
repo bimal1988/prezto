6881ec4b87d70b78b8ed9c3823fec68eef181623
bimal <bimal.behera@gmail.com>
2020-02-17 00:22:44 +0530

    fzf init file modified

--- modules/fzf/init.zsh
+++ modules/fzf/init.zsh
@@ -8,19 +8,80 @@
 # Load dependencies.
 pmodload 'editor'
 
+# Add manually installed fzf to path
+if [[ -s "$HOME/.fzf/bin/fzf" ]]; then
+  path=("$HOME/.fzf/bin" $path)
+fi
+
+if (( ! $+commands[fzf] )); then
+  return 1
+fi
+
 # Source module files.
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+source "${0:h}/external/shell/key-bindings.zsh"
+source "${0:h}/external/shell/completion.zsh"
 
 # Set default options
-export FZF_DEFAULT_COMMAND="rg --files --hidden --smart-case --follow --glob '!.git/*'"
-export FZF_TMUX=1
+# Use fd or ripgrep or ag if available
+if (( $+commands[rg] )); then
+  export FZF_DEFAULT_COMMAND="fd --type f"
+  _fzf_compgen_path() {
+    fd --type f "$1"
+  }
+elif (( $+commands[rg] )); then
+  export FZF_DEFAULT_COMMAND="rg --files --smart-case --glob '!.git/*'"
+  _fzf_compgen_path() {
+    rg --files "$1"
+  }
+elif (( $+commands[ag] )); then
+  export FZF_DEFAULT_COMMAND="ag -g ''"
+  _fzf_compgen_path() {
+    ag -g '' "$1"
+  }
+fi
+
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+# Show preview on Ctrl-T
 export FZF_CTRL_T_OPTS="--preview '(bat --style=numbers --color=always {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
-export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
-export FZF_COMPLETION_TRIGGER=',,'
+
+# Show directory content on ALT-C, if tree command is installed
+if (( $+commands[tree] )); then
+  export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
+fi
+
+# Integration with tmux
+export FZF_TMUX=1
+export FZF_TMUX_HEIGHT=40%
+
+# Integration with z
+if zstyle -t ':prezto:module:z' loaded; then
+  unalias z 2> /dev/null
+
+  z() {
+    if [[ -z "$*" ]]; then
+      cd "$(_z -l 2>&1 | fzf +s --tac | sed 's/^[0-9,.]* *//')"
+    else
+      _last_z_args="$@"
+      _z "$@"
+    fi
+  }
+
+  zz() {
+    cd "$(_z -l 2>&1 | sed 's/^[0-9,.]* *//' | fzf -q "$_last_z_args")"
+  }
+
+  #
+  # Aliases
+  #
+  alias j=z
+  alias jj=zz
+fi
 
 #
 # Key Bindings
 #
+export FZF_COMPLETION_TRIGGER=',,'
 
 if [[ -n "$key_info" ]]; then
   # vi
