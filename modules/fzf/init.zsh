97698c99cbc0f47fc83323ee5ee593b77f62adf0
bimal <bimal.behera@gmail.com>
2020-02-17 00:53:42 +0530

    fzf-tmux used for z

--- modules/fzf/init.zsh
+++ modules/fzf/init.zsh
@@ -60,7 +60,7 @@ if zstyle -t ':prezto:module:z' loaded; then
 
   function z() {
     if [[ -z "$*" ]]; then
-      cd "$(_z -l 2>&1 | fzf +s --tac | sed 's/^[0-9,.]* *//')"
+      cd "$(_z -l 2>&1 | fzf-tmux +s --tac | sed 's/^[0-9,.]* *//')"
     else
       _last_z_args="$@"
       _z "$@"
@@ -68,7 +68,7 @@ if zstyle -t ':prezto:module:z' loaded; then
   }
 
   function zz() {
-    cd "$(_z -l 2>&1 | sed 's/^[0-9,.]* *//' | fzf -q "$_last_z_args")"
+    cd "$(_z -l 2>&1 | sed 's/^[0-9,.]* *//' | fzf-tmux -q "$_last_z_args")"
   }
 
   #
